26fea872e6e5ddb8d4da906c70ce745f561603d2
DA20060107 <s24g2133tv@chibatech.ac.jp>
2026-06-26 13:05:14 +0900

    Update employee_registar.php

--- pages/employee/employee_registar.php
+++ pages/employee/employee_registar.php
@@ -1,3 +1,8 @@
+<?php
+// セッション開始（必要なら）
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,15 +12,20 @@
 <body>
     <h1>従業員 新規登録</h1>
 
-    <form action="employee_registar_action.php" method="POST">
+    <form action="./employee_registar_action.php" method="POST">
         <label>名前：</label>
         <input type="text" name="name" required><br><br>
 
-        <label>メールアドレス：</label>
-        <input type="email" name="email" required><br><br>
+        <label>ログインID:</label>
+        <input type="text" id="username" name="username" placeholder="例: employee06" required><br><br>
 
+        
         <label>パスワード：</label>
         <input type="password" name="password" required><br><br>
+    
+
+        <label>メールアドレス：</label>
+        <input type="email" name="email" required><br><br>
 
         <button type="submit">登録する</button>
     </form>
